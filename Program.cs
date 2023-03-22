a25708f90cbfd557441d160b9ef0ac2e1cfe4693
Aspirin493 <49791856+Aspirin493@users.noreply.github.com>
2023-03-22 16:32:03 +0400

    project created

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace easy_translator
+{
+    static class Program
+    {
+        /// <summary>
+        /// Main entrypoint
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
